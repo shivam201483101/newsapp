0847d5b414552a088b4e5f0b8e3490917f2ff822
Shivam Pandey <shivam.201483101@vcet.edu.in>
2024-03-30 14:01:33 +0530

    Update settings.py

--- newsapp/settings.py
+++ newsapp/settings.py
@@ -84,12 +84,12 @@ WSGI_APPLICATION = 'newsapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
@@ -128,9 +128,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 # STATICFILES_DIRS=[BASE_DIR,'static']
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-MEDIA_URLS ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# MEDIA_URLS ='/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
